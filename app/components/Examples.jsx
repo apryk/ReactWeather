e1ddf866cbef9f7d07a66f8318ca697406eb2dfb
apryk <aleksandr.prykhodko@gmail.com>
2017-03-12 16:22:19 +1100

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
 
 var Examples = (props) => { 
-        return (
-            <div>
-                <h2>Examples component!</h2>
-            </div>
-        );    
+    return (
+        <div>
+            <h2>Examples component!</h2>
+            <h3>Welcome to Examples page</h3>
+        </div>
+    );    
 };
 module.exports = Examples;
