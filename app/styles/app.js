095a9ecab6123103895809c30bdbbdb9520ce662
apryk <aleksandr.prykhodko@gmail.com>
2017-03-14 21:53:21 +1100

    Switch to Sass styling

--- app/styles/app.js
+++ app/styles/app.js
@@ -3,7 +3,8 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   'page-title': {
     'marginTop': [{ 'unit': 'rem', 'value': 2.5 }],
-    'marginBottom': [{ 'unit': 'rem', 'value': 2.5 }]
+    'marginBottom': [{ 'unit': 'rem', 'value': 2.5 }],
+    'color': '#555'
   },
   'input[type=search]': {
     'boxShadow': [{ 'unit': 'string', 'value': 'none' }, { 'unit': 'string', 'value': 'none' }, { 'unit': 'string', 'value': 'none' }, { 'unit': 'string', 'value': 'none' }]
